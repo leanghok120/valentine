06cff99853a6ba92368ab936923d7a9e77c71bae
leanghok <leanghokoeng5@gmail.com>
2025-02-02 14:39:27 +0700

    add a success page

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
+const header = document.getElementById("header");
+const img = document.getElementById("img");
 
 let width = 70;
 let height = 50;
 
-function handleClick() {
+function handleYes() {
+  header.textContent = "YAYYYYYY I LOVE YOU POOKIE :3";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  img.src = "./bear-kiss-bear-kisses.gif";
+}
+
+function handleNo() {
   width *= 1.4;
   height *= 1.4;
   yesBtn.style.width = `${width}px`;
